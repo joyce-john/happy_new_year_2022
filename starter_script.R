2edc8d3c138a543107dd79fc74dd0a0a5c4003d7
John Joyce <joyce_john@student.ceu.edu>
2021-12-26 23:18:37 -0500

    switched to scale_y_reverse

--- starter_script.R
+++ starter_script.R
@@ -6,16 +6,24 @@ library(gifski) # gganimate requirement
 library(png) # gganimate requirement 
 
 # create data frame of dots. line breaks loosely indicate different sections of the letters or changes in pen stroke when writing the letters
-df <- data.frame("x" = c(33, 82, 87, 87, 81,
+df <- data.frame("x" = c(33, 82, 87, 87, 81, # start H
                          95, 131, 165,
                          196, 204, 206, 208,
-                         208, 225, 245 
+                         208, 225, 245, # end H
+                         257, 283, 307, 330, # start A
+                         344, 360, 378,
+                         292, 325, 355 # end A
                          ),
-                 "y" = c(378, 328, 290, 235, 166,
+                 "y" = c(378, 328, 290, 235, 166, # start H
                          268, 271, 272,
                          255, 231, 194, 162,
-                         326, 362, 372),
-                 "symbol" = c(rep("H", 15)))
+                         326, 362, 372, # end H
+                         303, 269, 233, 192, # start A
+                         231, 265, 304,
+                         248, 249, 247 # end A
+                         ),
+                 "symbol" = c(rep("H", 15),
+                              rep("A", 10)))
 
 df$point_number <- seq(1, nrow(df))
 
@@ -27,19 +35,22 @@ plot(df)
 ggplot(data = df, aes(x = x, y = y)) +
   geom_point() +
   geom_line() +
-  xlim(0,500) +
+  xlim(0,1000) +
   ylim(0,1000) +
-  scale_y_discrete(limits = "rev")
+  scale_y_reverse()
+
 
 
 
 animation <-
 ggplot(data = df, aes(x = x, y = y)) +
   geom_point() +
-  geom_smooth(color = "red") +
-  xlim(0,500) +
+  geom_line(color = "red") +
+  xlim(0,1000) +
   ylim(0,1000) +
-  scale_y_discrete(limits = "rev") +
-  transition_state(point_number)
+  scale_y_reverse() +
+  transition_reveal(point_number)
 
 animate(animation)
+
+## TODO: add another class of points with a an alpha of 0.00 to serve as invisible connectors between letters
