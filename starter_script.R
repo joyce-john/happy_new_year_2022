b8c13b6cf448319757e9deefc54e31180a4d47e2
John Joyce <joyce_john@student.ceu.edu>
2021-12-28 20:30:43 -0500

    reveal letters one by one, keeping old values with group in aes

--- starter_script.R
+++ starter_script.R
@@ -6,55 +6,30 @@ library(gifski) # gganimate requirement
 library(png) # gganimate requirement 
 
 # create data frame of dots. line breaks loosely indicate different sections of the letters or changes in pen stroke when writing the letters
-df <- data.frame("x" = c(33, 82, 87, 87, 81, # start H
-                         95, 131, 165,
-                         196, 204, 206, 208,
-                         208, 225, 245, # end H
-                         257, 283, 307, 330, # start A
-                         344, 360, 378,
-                         292, 325, 355 # end A
-                         ),
-                 "y" = c(378, 328, 290, 235, 166, # start H
-                         268, 271, 272,
-                         255, 231, 194, 162,
-                         326, 362, 372, # end H
-                         303, 269, 233, 192, # start A
-                         231, 265, 304,
-                         248, 249, 247 # end A
-                         ),
-                 "symbol" = c(rep("H", 15),
-                              rep("A", 10)))
+df <- data.frame("greeting" = c("H",
+                                "A",
+                                "P",
+                                "P",
+                                "Y",
+                                " N",
+                                "E",
+                                "W")
+                 )
 
 # sequential plotting order for points
-df$point_number <- seq(1, nrow(df))
+df$frame_number <- seq(1, nrow(df))
 
-# calculate values for drawing a smoothed loess line outside of ggplot
-# inspiration from https://stackoverflow.com/questions/60856938/animate-points-and-regression-line-along-date-with-gganimate
-df$smoothed_y_values <- predict(loess(y ~ x, df))
-
-# sample plot
-plot(df)
-
-
-# ggplot of points with geom_smooth() drawing lines between dots to animate handwriting
-ggplot(data = df, aes(x = x, y = y)) +
-  geom_point() +
-  geom_line() +
-  xlim(0,1000) +
-  ylim(0,1000) +
-  scale_y_reverse()
-
-
-
+# coordinate locations for text. leaving some empty space on the edges of the frame as padding
+df$x <- seq(from = 0.2, to = 0.8, length.out = nrow(df))
+df$y <- seq(from = 0.2, to = 0.8, length.out = nrow(df))
 
 animation <-
 ggplot(data = df, aes(x = x, y = y)) +
-  geom_point() +
-  geom_line(color = "red") +
-  xlim(0,1000) +
-  ylim(0,1000) +
-  scale_y_reverse() +
-  transition_reveal(point_number)
+  geom_text(aes(label = greeting, group = frame_number), size = 12) + 
+  xlim(0,1) +
+  ylim(0,1) +
+  transition_reveal(along = frame_number) +
+  shadow_mark()
 
 animate(animation)
 
